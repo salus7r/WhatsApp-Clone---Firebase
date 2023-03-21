eab1d8557b8ba84e7ea4606a8df7912b49f1201d
Salman Zahid Latif <salu_s7r@yahoo.com>
2023-03-21 14:15:05 +0500

    added firebase

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyBXwnu0Z6Nrs49k9fRdci2HmYVI97qC-8k",
+	authDomain: "whatsapp-clone-s7r.firebaseapp.com",
+	projectId: "whatsapp-clone-s7r",
+	storageBucket: "whatsapp-clone-s7r.appspot.com",
+	messagingSenderId: "490603281188",
+	appId: "1:490603281188:web:5e86bff381a04f1bbb51b1",
+	measurementId: "G-344C3R5F0H",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
