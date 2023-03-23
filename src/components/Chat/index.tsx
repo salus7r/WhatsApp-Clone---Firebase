c32f487985561c8b450dbef87ac93c645d7d3ae5
Salman Zahid Latif <salu_s7r@yahoo.com>
2023-03-23 14:05:19 +0500

    added chat container with the header styles + utilized custom hook for random avatar

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Avatar, IconButton } from "@mui/material";
+import { AttachFile, MoreVert, SearchOutlined } from "@mui/icons-material";
 
+import useSeedAvatar from "./../../hooks/useSeedAvatar";
 import "./chat.css";
 
-type Props = {}
+type Props = {};
 
-const Chat = (props: Props) => {
-    return (
-        <div className="chat"></div>
-    )
-}
+const Chat: React.FC<Props> = (props) => {
+	const seed = useSeedAvatar();
 
-export default Chat
+	return (
+		<div className="chat">
+			<div className="chat__header">
+				<Avatar src={seed} />
+				<div className="chat__headerInfo">
+					<h3>Room Name</h3>
+					<p>Last seen at...</p>
+				</div>
+				<div className="chat__headerRight">
+					<IconButton title={"Search"}>
+						<SearchOutlined />
+					</IconButton>
+					<IconButton title={"Attachments"}>
+						<AttachFile />
+					</IconButton>
+					<IconButton title={"More"}>
+						<MoreVert />
+					</IconButton>
+				</div>
+			</div>
+			<div className="chat__body"></div>
+			<div className="chat__footer"></div>
+		</div>
+	);
+};
+
+export default Chat;
