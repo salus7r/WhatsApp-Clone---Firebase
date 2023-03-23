3053c1466145ac3ac95dd6463bd5cd99fd05281a
Salman Zahid Latif <salu_s7r@yahoo.com>
2023-03-23 14:03:54 +0500

    added sidebar components & styles

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,24 +1,52 @@
-import React from 'react'
-import { Avatar } from "@mui/material"
-import { Chat, DonutLarge, MoreVert } from "@mui/icons-material"
+import React from "react";
+import { Avatar, IconButton } from "@mui/material";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@mui/icons-material";
 
-type Props = {}
+import SidebarChat from "../SidebarChat";
+import "./sidebar.css";
 
-const Sidebar = (props: Props) => {
-    return (
-        <div className="sidebar">
-            <div className="sidebar__header">
-                <Avatar />
-                <div className="sidebar__headerRight">
-                    <DonutLarge />
-                    <Chat />
-                    <MoreVert />
-                </div>
-            </div>
-            <div className="sidebar__search"></div>
-            <div className="sidebar__chats"></div>
-        </div>
-    )
-}
+type Props = {};
 
-export default Sidebar
+const Sidebar: React.FC<Props> = (props) => {
+	return (
+		<div className="sidebar">
+			<div className="sidebar__header">
+				<Avatar />
+				<div className="sidebar__headerRight">
+					<IconButton title={"Statuses"}>
+						<DonutLarge />
+					</IconButton>
+					<IconButton title={"Chats"}>
+						<Chat />
+					</IconButton>
+					<IconButton title={"More"}>
+						<MoreVert />
+					</IconButton>
+				</div>
+			</div>
+			<div className="sidebar__search">
+				<div className="sidebar__searchContainer">
+					<SearchOutlined />
+					<input placeholder="Search or start new chat" type="text" />
+				</div>
+			</div>
+			<div className="sidebar__chats">
+				<SidebarChat addNewChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
