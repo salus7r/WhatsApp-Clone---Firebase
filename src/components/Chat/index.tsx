36f73e759e0ecfb49ddfe5bbc1aa6fb301363bab
Salman Zahid Latif <salu_s7r@yahoo.com>
2023-03-24 11:32:29 +0500

    chat section updated + message UI added

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Avatar, IconButton } from "@mui/material";
-import { AttachFile, MoreVert, SearchOutlined } from "@mui/icons-material";
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from "@mui/icons-material";
 
 import useSeedAvatar from "./../../hooks/useSeedAvatar";
 import "./chat.css";
@@ -9,6 +9,8 @@ type Props = {};
 
 const Chat: React.FC<Props> = (props) => {
 	const seed = useSeedAvatar();
+
+	const sendMessage = useCallback(() => {}, []);
 
 	return (
 		<div className="chat">
@@ -30,8 +32,28 @@ const Chat: React.FC<Props> = (props) => {
 					</IconButton>
 				</div>
 			</div>
-			<div className="chat__body"></div>
-			<div className="chat__footer"></div>
+			<div className="chat__body">
+				<p className="chat__message">
+					<span className="chat__name">Salman Zahid Latif</span>
+					Hey Guys...
+					<span className="chat__timeStamp">3:52pm</span>
+				</p>
+				<p className={`chat__message ${true && "chat__receiver"}`}>
+					<span className="chat__name">Salman Zahid Latif</span>
+					Hey Guys...
+					<span className="chat__timeStamp">3:52pm</span>
+				</p>
+			</div>
+			<div className="chat__footer">
+				<InsertEmoticon />
+				<form>
+					<input type="text" placeholder="Type a message" />
+					<button type="submit" onClick={sendMessage}>
+						Send a message
+					</button>
+				</form>
+				<Mic />
+			</div>
 		</div>
 	);
 };
