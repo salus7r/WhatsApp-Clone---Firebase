2de6124d7d23505c64e44bdf513ac078c357e403
Salman Zahid Latif <salu_s7r@yahoo.com>
2023-03-27 14:31:12 +0500

    updated see hook for changing avatar on every room id change

--- src/hooks/useSeedAvatar.ts
+++ src/hooks/useSeedAvatar.ts
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-export default function useSeedAvatar() {
+export default function useSeedAvatar(roomId?: string) {
 	const [seed, setSeed] = useState(0);
 
 	useEffect(() => {
 		setSeed(Math.floor(Math.random() * 5000));
-	}, []);
+	}, [roomId]);
 
 	return `https://avatars.dicebear.com/api/human/${seed}.svg`;
 }
